8a226658a9854d13dbd8eb9da7a8a670ccbe871d
Debaraj Barua <debaraj.barua@smail.inf.h-brs.de>
2017-05-10 00:25:24 +0200

    Initializing Project

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Daniel Vazquez
+ * Aritificial Intelligence for Robotics
+ * SS 2016
+ * Assignment 5
+ *
+ * main.cpp
+ * */
+
+#include "environment.hpp"
+
+int main(int arc, char* argv[])
+
+{
+    Environment anEnvironment;
+    anEnvironment.run();
+    return 1;
+}
